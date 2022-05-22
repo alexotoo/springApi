ac8f0b6a844daf9bd05387035d57d505b3787f2e
Alex Otoo <alexotoous@outlook.com>
2022-05-22 02:04:09 -0400

    new student added

--- src/main/java/com/example/springApi/SpringApiApplication.java
+++ src/main/java/com/example/springApi/SpringApiApplication.java
@@ -1,10 +1,13 @@
 package com.example.springApi;
 
+import com.example.springApi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,10 @@ public class SpringApiApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of( "hello ","Java", "spring");
+	public List<Student> hello(){
+		return List.of(
+				new Student(1L,"Ama","ama@gmail.com", LocalDate.of(2000, Month.JANUARY,5),21)
+		);
 	}
 
 }
